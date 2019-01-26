c2f551e2d14364db1a96e65df9d7437af0568c30
SPAjs <spajsorg@gmail.com>
2019-01-26 21:51:39 +0530

    v1.5.2 Enable CORS in live-server

--- spa-cli.js
+++ spa-cli.js
@@ -109,6 +109,7 @@ function liveServerStart( cbFn ){
                 host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
                 root: path.resolve(appName), // Set root directory that's being served. Defaults to cwd.
                 browser: defBrowser,
+                cors: true,  //Enable CORS
                 open: true, // When false, it won't load your browser by default.
                 ignore: '', // comma-separated string for paths to ignore
                 file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
